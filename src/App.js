2cbb5a8184d4803e5c68dcee7b18006e09f34e03
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2025-12-09 21:38:05 +0100

    tabs

--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ function Tabbed({ content }) {
   return (
     <div>
       <div className="tabs">
-        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={0} activeTab={activeTab} 
+        onClick={setActiveTab} />
         <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
